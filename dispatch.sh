39b16406e7b0ddbcf9d5712d7d523e7900a3e9e5
Sumner Hearth <sumnerh@scc1.bu.edu>
2020-04-01 23:45:13 -0400

    Updates

--- dispatch.sh
+++ dispatch.sh
@@ -12,14 +12,15 @@ else
 	mkdir -p $BASE_DIR
 	mkdir -p "$BASE_DIR/code"
 	cp *.py "$BASE_DIR/code"
-	cp run.sh "$BASE_DIR/code"
+	cp *.sh "$BASE_DIR/code"
 	# Speeds things up sometimes
 	cp -r graphcache "$BASE_DIR/code"
 	cd "$BASE_DIR/code"
 	echo "Working directory: $(pwd)"
-	echo "qsub -wd \"$BASE_DIR/code\" -t 1-$NUM_SHARDS run.sh $BASE_DIR"
-	OUTPUT=$(qsub -wd "$BASE_DIR/code" -t 1-$NUM_SHARDS run.sh $BASE_DIR)
+	echo "qsub -wd \"$BASE_DIR/code\" -t 1-$NUM_SHARDS run.sh \"$BASE_DIR\""
+	OUTPUT=$(qsub -wd "$BASE_DIR/code" -t 1-$NUM_SHARDS run.sh "$BASE_DIR")
 	echo $OUTPUT
-	JOB_ID=$(echo $OUTPUT | grep -E -o "[0-9]+"sub -wd "$BASE_DIR/code" -t 1-$NUM_SHARDS run.sh $BASE_DIR
-	qsub -wd "$BASE_DIR/code" -hold_jid $JOB_ID run_analysis.sh $BASE_DIR
+	JOB_ID=$(echo $OUTPUT | grep -E -o "[0-9]+" | head -1)
+	echo "qsub -wd \"$BASE_DIR/code\" -hold_jid $JOB_ID run_analysis.sh \"$BASE_DIR\""
+	qsub -wd "$BASE_DIR/code" -hold_jid $JOB_ID run_analysis.sh "$BASE_DIR"
 fi
--- experiment_gen.py
+++ experiment_gen.py
@@ -8,13 +8,13 @@ import numpy
 
 
 def experiment_generator(base_dir):
-    n = 10
-    betas = 10**numpy.linspace(-2,0,n)
+    n = 20
+    betas = 10**numpy.linspace(-2,0, n)
+    h = 0.0
+    j = 1.0
+    gamma = 1.0
     for i, beta in enumerate(betas):
         print("Building experiment {}".format(i))
-        h = 0.0  # float(i) / n
-        j = 1.0
-        transverse = 1.0
         experiment_dir = os.path.join(base_dir, "experiment_{}".format(i))
         print("\tUsing directory: {}".format(experiment_dir))
         config = ExperimentConfig(experiment_dir,
@@ -23,8 +23,8 @@ def experiment_generator(base_dir):
                                                   'thermalization_time': 1e6,
                                                   'timesteps': 1e7 + 1e6,
                                                   'sampling_freq': 1e3},
-                                  h=h, j=j, gamma=transverse, throw_errors=True)
-        config.num_reads = 10
+                                  h=h, j=j, gamma=gamma, throw_errors=True)
+        config.num_reads = 10000
         config.auto_scale = False
-        config.build_graph(min_x=7, max_x=15, min_y=0, max_y=8)
+        config.build_graph(min_x=7, max_x=15, min_y=0, max_y=8, build_kwargs={'ideal_periodic_boundaries': True})
         yield config
