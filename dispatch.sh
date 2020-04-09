e349f734f6c4ebc18c576c53263d675085d64ede
sumnernh@gmail.com <sumnernh@gmail.com>
2020-04-09 15:58:33 -0400

    Shell cleanup

--- dispatch.sh
+++ dispatch.sh
@@ -9,18 +9,18 @@ else
 	echo "Running into directory $BASE_DIR with $NUM_SHARDS shards"
 
 
-	mkdir -p $BASE_DIR
+	mkdir -p "$BASE_DIR"
 	mkdir -p "$BASE_DIR/code"
 	cp *.py "$BASE_DIR/code"
 	cp *.sh "$BASE_DIR/code"
 	# Speeds things up sometimes
 	cp -r graphcache "$BASE_DIR/code"
-	cd "$BASE_DIR/code"
+	cd "$BASE_DIR/code" || exit
 	echo "Working directory: $(pwd)"
 	echo "qsub -wd \"$BASE_DIR/code\" -t 1-$NUM_SHARDS run.sh \"$BASE_DIR\""
-	OUTPUT=$(qsub -wd "$BASE_DIR/code" -t 1-$NUM_SHARDS run.sh "$BASE_DIR")
-	echo $OUTPUT
-	JOB_ID=$(echo $OUTPUT | grep -E -o "[0-9]+" | head -1)
+	OUTPUT=$(qsub -wd "$BASE_DIR/code" -t 1-"$NUM_SHARDS" run.sh "$BASE_DIR")
+	echo "$OUTPUT"
+	JOB_ID=$(echo "$OUTPUT" | grep -E -o "[0-9]+" | head -1)
 	echo "qsub -wd \"$BASE_DIR/code\" -hold_jid $JOB_ID run_analysis.sh \"$BASE_DIR\""
-	qsub -wd "$BASE_DIR/code" -hold_jid $JOB_ID run_analysis.sh "$BASE_DIR"
+	qsub -wd "$BASE_DIR/code" -hold_jid "$JOB_ID" run_analysis.sh "$BASE_DIR"
 fi
