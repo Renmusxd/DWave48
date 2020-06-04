4a51a9a79d880edb74bf6acbd5b673ec5463b5f0
sumnernh@gmail.com <sumnernh@gmail.com>
2020-06-04 17:21:49 -0400

    Moved rust packages to their own repos

--- monte_carlo_core/src/lib.rs
+++
@@ -1,6 +0,0 @@
-///
-/// `ising_monte_carlo` is a library for simulating classical and quantum ising systems on a lattice
-/// using monte carlo methods.
-///
-pub mod graph;
-pub mod sse;
--- monte_carlo_core/src/sse/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod arena;
-pub mod fast_ops;
-pub mod qmc_graph;
-pub mod qmc_traits;
-pub mod qmc_types;
-pub mod simple_ops;
