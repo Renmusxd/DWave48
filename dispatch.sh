26e8d19e99c53e48f1fadbc07c63a4d4e3dcbcc0
sumnernh@gmail.com <sumnernh@gmail.com>
2020-06-15 13:38:15 -0400

    Fixed slots

--- dispatch.sh
+++ dispatch.sh
@@ -6,6 +6,8 @@ if [ -z "$BASE_DIR" ]; then
 else
 
 	NUM_SHARDS=${2:-10}
+	NUM_SLOTS=${3:-1}
+	NUM_ANALYZE_SLOTS=${4:-10}
 	echo "Running into directory $BASE_DIR with $NUM_SHARDS shards"
 
 
@@ -18,8 +20,8 @@ else
 	cp -r graphcache "$BASE_DIR/code"
 	cd "$BASE_DIR/code" || exit
 	echo "Working directory: $(pwd)"
-	echo "qsub -pe omp \"$NUM_ANALYZE_SLOTS\" -wd \"$BASE_DIR/code\" -t 1-$NUM_SHARDS run.sh \"$BASE_DIR\" \"$NUM_SHARDS\""
-	OUTPUT=$(qsub -pe omp "$NUM_ANALYZE_SLOTS" -wd "$BASE_DIR/code" -t 1-"$NUM_SHARDS" run.sh "$BASE_DIR" "$NUM_SHARDS")
+	echo "qsub -pe omp \"NUM_SLOTS\" -wd \"$BASE_DIR/code\" -t 1-$NUM_SHARDS run.sh \"$BASE_DIR\" \"$NUM_SHARDS\""
+	OUTPUT=$(qsub -pe omp "NUM_SLOTS" -wd "$BASE_DIR/code" -t 1-"$NUM_SHARDS" run.sh "$BASE_DIR" "$NUM_SHARDS")
 	echo "$OUTPUT"
 	JOB_ID=$(echo "$OUTPUT" | grep -E -o "[0-9]+" | head -1)
 	echo "qsub -pe omp \"$NUM_ANALYZE_SLOTS\" -wd \"$BASE_DIR/code\" -hold_jid $JOB_ID run_analysis.sh \"$BASE_DIR\""
